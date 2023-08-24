64303be40daf41cc8316981adae74995da84b14b
AndresAndrada <andresandrada1994@gmail.com>
2023-08-24 13:46:41 -0300

    Probando commit

--- src/controllers/LoginControllers/LoginController.js
+++ src/controllers/LoginControllers/LoginController.js
@@ -2,15 +2,14 @@ const { User } = require("../../DB");
 const comparepassword = require("../../utils/comparepassword");
 module.exports = async ({ email, password }) => {
   const existUser = await User.findOne({ where: { email: email } });
-  const isCorrectPassword=await comparepassword(password,existUser.password);
-  
-  if(!existUser || !isCorrectPassword){
-    throw new Error;
-  }else{
+  const isCorrectPassword = await comparepassword(password, existUser.password);
 
+  if (!existUser || !isCorrectPassword) {
+    throw new Error();
+  } else {
     return {
-      token:'any',
-      user:existUser
-    }
+      token: "any",
+      user: existUser,
+    };
   }
 };
