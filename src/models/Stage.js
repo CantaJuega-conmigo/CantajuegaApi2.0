424c91e8bfa09683131b613c92dd4e88ebca09d5
unknown <joakig6@gmail.com>
2023-08-29 00:47:38 -0300

    fix last merge

--- src/models/Stage.js
+++ src/models/Stage.js
@@ -1,5 +1,4 @@
 const { DataTypes } = require("sequelize");
-<<<<<<< HEAD
 
 module.exports = (sequelize) => {
   // defino el modelo
@@ -18,55 +17,24 @@ module.exports = (sequelize) => {
       },
       description: {
         type: DataTypes.TEXT,
+        allowNull: false,
       },
       minAge: {
         type: DataTypes.INTEGER,
+        allowNull: false,
+        // unique: true,
       },
       maxAge: {
         type: DataTypes.INTEGER,
+        allowNull: false,
       },
       child: {
-        type: DataTypes.ARRAY(DataTypes.STRING)
-      }
+        type: DataTypes.ARRAY(DataTypes.JSONB),
+        allowNull: true,
+      },
     },
     {
       timestamps: false,
     }
-  );
-=======
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define("Stage", {
-    //
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    minAge: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      unique: true,
-    },
-    maxAge: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    child: {
-      type: DataTypes.ARRAY(DataTypes.JSONB),
-      allowNull: true,
-    },
-  });
->>>>>>> Axel-branch
-};
+  )
+}
