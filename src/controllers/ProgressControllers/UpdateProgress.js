65e35ec65b8ed5f69e4415ba78b993b6d7e0e9db
unknown <joakig6@gmail.com>
2023-09-04 03:10:20 -0300

    coment progress code

--- src/controllers/ProgressControllers/UpdateProgress.js
+++ src/controllers/ProgressControllers/UpdateProgress.js
@@ -7,23 +7,23 @@ module.exports = async (id, newData, select) => {
     const isChildExists=await getUsers(ActualProgress.ChildId)
     const isCorrectSelect=select in ActualProgress
     
-    if(!isChildExists){
+    if(!isChildExists){//si no existe el child tiramos error
       throw new Error('Child not exists.')
     }
     
-    if(!isCorrectSelect){
+    if(!isCorrectSelect){//si no existe la propiedad en la tabla progress tiramos error
       throw new Error('Error in the server')
     }
-    const validateAtributtes= validateProgressSection(newData,select)
-    if(!validateAtributtes){
+    const validateAtributtes= validateProgressSection(newData,select)//devuelve true/false o genera errores.
+    if(!validateAtributtes){//validamos que la informacion que llega tenga atributos validos dependiendo la informacion a cambiar
       throw new Error('Error in the server')
     }
-    const newProgress = {
+    const newProgress = {///y si todo va bien hacemos cambios
       ...ActualProgress,
       [select]: newData,
     };
 
-    const updateProgress = await Progress.update(newProgress, {
+    const updateProgress = await Progress.update(newProgress, {///guardamos los cambios en la db
       where: {
         id: id,
       },
