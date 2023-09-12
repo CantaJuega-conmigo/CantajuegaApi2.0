84faf717d33c7f1615aa9f612fd15d3739e4d8a5
unknown <joakig6@gmail.com>
2023-09-12 18:07:04 -0300

    add enviroment PORT variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./src/server");
 const { conn } = require('./src/DB');
-const PORT=3001
+const {PORT} = process.env;
 
 conn.sync({ force: false }).then(() => {
     server.listen(PORT, () => {
