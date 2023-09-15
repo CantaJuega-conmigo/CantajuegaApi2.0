e4e168e613b59860b8fea6ab438d71645b03da63
unknown <joakig6@gmail.com>
2023-09-15 01:09:23 -0300

    change response in logouthandler

--- src/handlers/LogOutHandler/logoutHandler.js
+++ src/handlers/LogOutHandler/logoutHandler.js
@@ -1,11 +1,14 @@
 module.exports=(req,res)=>{
  try {
     res.clearCookie('accesscookie')
-    res.send('Se ha deslogueado con exito.')
+    res.send({
+      error:false,
+      message:'Se ha deslogueado con exito'
+    })
  } catch (error) {
     res.status(401).send({
         error:true,
         message:error.message??'server failed.'
     })
- }
+ } 
 }
