24fe0c0bac0e2ac9b68b26c95a2a3d76bcff6e51
unknown <joakig6@gmail.com>
2023-09-17 13:47:25 -0300

    update changeUserPassword controller to use in others handlers

--- src/controllers/UserControllers/changeUserPassword.js
+++ src/controllers/UserControllers/changeUserPassword.js
@@ -1,13 +1,16 @@
 const { User } = require("../../DB");
 const comparepassword = require("../../utils/comparepassword");
-module.exports = async (email, newpassword, password) => {
+module.exports = async (email, newpassword, password, recover) => {
   try {
-    if (!email || !newpassword || !password)
-      throw new Error("Error in server.");
+    if (!email || !newpassword) throw new Error("Error in server.");
     const user = await User.findOne({ where: { email: email } });
     if (!user) throw new Error("User not found");
-    const passwordIsValid = await comparepassword(password, user.password);
-    if (!passwordIsValid) throw new Error("La contraseña es incorrecta");
+
+    if (!recover) {
+      if (!password) throw new Error("Error in server.");
+      const passwordIsValid = await comparepassword(password, user.password);
+      if (!passwordIsValid) throw new Error("La contraseña es incorrecta");
+    }
     const resquest = await User.update(
       { password: newpassword },
       { where: { email: email } }
