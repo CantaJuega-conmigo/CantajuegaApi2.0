15eeddea5ccbd6f8e39fc1769ea9b1351944071f
unknown <joakig6@gmail.com>
2023-09-17 16:44:31 -0300

    update fake info in fakememberships seed

--- src/seed/fakeMemberships.js
+++ src/seed/fakeMemberships.js
@@ -3,36 +3,33 @@ module.exports= [
     {
         id: uuidv4(),
         name: 'Mensual',
-        description: '⦁	Todas las herramientas terapéuticas para niñas y niños de 0 a 6 años',
+        description: 'Todas las herramientas terapéuticas para niñas y niños de 0 a 6 años.',
         price: 49.99,
         duration: 1,
-        text1:'texto 1',
-        text2: "texto2",
-        text3: "texto3",
+        text1:'Materias de apoyo audiovisual.',
+        text2: "Canciones del programa.",
         recurrenteId:'prod_6sl3mnqq'
         // recurrenteId:'prod_wmqaobmf'
     },
     {
         id: uuidv4(),
         name: 'Semestral',
-        description: '⦁	Todas las herramientas terapéuticas para niñas y niños de 0 a 6 años',
+        description: 'Todas las herramientas terapéuticas para niñas y niños de 0 a 6 años.',
         price: 199.99,
         duration: 6,
-        text1:'texto 1',
-        text2: "texto2",
-        text3: "texto3",
+        text1:'Materias de apoyo audiovisual.',
+        text2: "Canciones del programa.",
         recurrenteId:'prod_6sl3mnqq'
         // recurrenteId:'prod_wmqaobmf'
     },
     {
         id: uuidv4(),
         name: 'anual',
-        description: '⦁	Todas las herramientas terapéuticas para niñas y niños de 0 a 6 años',
+        description: 'Todas las herramientas terapéuticas para niñas y niños de 0 a 6 años.',
         price: 299.99,
         duration: 12,
-        text1:'texto 1',
-        text2: "texto2",
-        text3: "texto3",
+        text1:'Materias de apoyo audiovisual.',
+        text2: "Canciones del programa.",
         recurrenteId:'prod_6sl3mnqq'
         // recurrenteId:'prod_wmqaobmf'
 
@@ -40,12 +37,11 @@ module.exports= [
     {
         id: uuidv4(),
         name: 'Canta conmigo',
-        description: '⦁	Todas las herramientas terapéuticas para niñas y niños de 0 a 6 años',
+        description: 'Todas las canciones infantiles por etapa de desarrollo.',
         price: 99.99,
         duration: 6,
-        text1:'texto 1',
-        text2: "texto2",
-        text3: "texto3",
+        text1:'Canciones digitales disponibles.',
+
         recurrenteId:'prod_6sl3mnqq'
         // recurrenteId:'prod_wmqaobmf'
     }
