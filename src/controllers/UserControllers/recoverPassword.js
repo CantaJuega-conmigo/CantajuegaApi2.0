47b244f2ccefc21f7521d079584f02d47aef39de
unknown <joakig6@gmail.com>
2023-09-17 04:07:44 -0300

    add controllers to recover paswword funcionality

---
+++ src/controllers/UserControllers/recoverPassword.js
@@ -0,0 +1,21 @@
+const { User } = require("../../DB");
+module.exports = async (email, code) => {
+  console.log(email, code);
+  try {
+    if (!email) throw new Error("Email is required.");
+    const userexists = await User.findOne({ where: { email: email } });
+    if (!userexists) throw new Error("El correo electronico no es valido.");
+    const codecreated = await User.update(
+      { OTPcode: code },
+      { where: { email: email } }
+    );
+
+    if (codecreated[0] !== 1)
+      throw new Error(
+        "Error al generar el codigo para actualizar la contraseña"
+      );
+    return true;
+  } catch (error) {
+    throw error;
+  }
+};
