2fc384ecf44c628b2c1a89722319094f7910e182
Wild-Design <brenneke_ruger@hotmail.com>
2023-09-19 20:31:26 -0300

    rearrange the model

--- src/models/Statistic.js
+++ src/models/Statistic.js
@@ -11,42 +11,34 @@ module.exports = async (sequelize) => {
       },
       Total_Users: {
         type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultvalue: 0,
+        defaultValue: 0,
       },
       Total_Stages: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         defaultValue: 0,
       },
       Total_Memberships: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         defaultValue: 0,
       },
       Total_Childs: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         defaultValue: 0,
       },
       Stages_Completed: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         defaultValue: 0,
       },
       Stages_In_Progress: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         defaultValue: 0,
       },
       Total_Reports: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         defaultValue: 0,
       },
       Memberships_Actives: {
         type: DataTypes.INTEGER,
-        allowNull: false,
         defaultValue: 0,
       },
     },
