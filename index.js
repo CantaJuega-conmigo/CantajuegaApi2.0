40d0fe70e084cdecf3274e33d23bc5dd3fb17d3a
unknown <joakig6@gmail.com>
2023-09-21 22:44:42 -0300

    force true to deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/server');
 const { conn } = require('./src/DB');
 const { PORT } = process.env;
 
-conn.sync().then(() => {
+conn.sync({force:true}).then(() => {
   server.listen(PORT, () => {
     console.log(`server open on port ${PORT}`); // eslint-disable-line no-console
   });
