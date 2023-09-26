aa4fdcc37e78714cf1bbf416208a62eaa5681a90
unknown <joakig6@gmail.com>
2023-09-26 17:12:17 -0300

    complete logical deletion in stages

--- src/controllers/StageControllers/logicalDeletionStage.js
+++ src/controllers/StageControllers/logicalDeletionStage.js
@@ -1,11 +1,13 @@
-const { Child } = require("../../DB");
+const { Stage } = require("../../DB");
 const logicalDeletion = require("../../helpers/logicalDeletion");
 module.exports = async (id) => {
+
   try {
-    const deleted = await logicalDeletion(Child, id, {
+    const deleted = await logicalDeletion(Stage, id, {
       message1: null,
       message2: null,
     });
+    console.log(deleted);
     return deleted;
   } catch (error) {
     throw new Error(
