5cac683f3c53a93e2631fd658ba302ec60ff3263
unknown <joakig6@gmail.com>
2023-10-11 00:23:15 -0300

    some changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const server = require('./src/server');
-// const { conn } = require('./src/DB');
+const { conn } = require('./src/DB');
 const { PORT } = process.env;
 
-// conn.sync({force:false}).then(() => {
+conn.sync({force:true}).then(() => {
   server.listen(PORT, () => {
     console.log(`server open on port ${PORT}`); // eslint-disable-line no-console
   });
-// });
+});
