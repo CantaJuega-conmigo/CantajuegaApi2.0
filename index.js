fc38d54581e9bb9bdfbbbed7d88b53278c6fc217
unknown <joakig6@gmail.com>
2023-10-10 21:59:21 -0300

    error forced to prove vercel

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const server = require('./src/server');
-const { conn } = require('./src/DB');
+// const { conn } = require('./src/DB');
 const { PORT } = process.env;
 
-conn.sync({force:true}).then(() => {
+conn.sync({force:false}).then(() => {
   server.listen(PORT, () => {
     console.log(`server open on port ${PORT}`); // eslint-disable-line no-console
   });
