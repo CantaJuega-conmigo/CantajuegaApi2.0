e82f03df21005e07083b54500164e63cc5499a10
unknown <joakig6@gmail.com>
2023-10-10 21:53:31 -0300

    try to deploy in vercel

--- src/DB.js
+++ src/DB.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST ,DB_NAME} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST ,DB_NAME,POSTGRES_USER,POSTGRES_HOST,POSTGRES_PASSWORD,POSTGRES_DATABASE} = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgres://${DB_USER??POSTGRES_USER}:${DB_PASSWORD??POSTGRES_PASSWORD}@${DB_HOST??POSTGRES_HOST}/${DB_NAME??POSTGRES_DATABASE}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
