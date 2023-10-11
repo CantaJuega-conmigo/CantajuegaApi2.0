5fed64da20040f925a4c4d531ec7d4f286dc5294
unknown <joakig6@gmail.com>
2023-10-10 22:52:10 -0300

    prueba xd

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
-const session = require('express-session');
+// const session = require('express-session');
 // require('./DB');
 const passport = require('./auth/google-auth');
 const rateLimit = require('express-rate-limit');
@@ -22,12 +22,12 @@ server.use(
   })
 );
 server.use(cookieParser());
-server.use(
-  session({ secret: 'secreto', resave: true, saveUninitialized: true })
-);
+// server.use(
+//   session({ secret: 'secreto', resave: true, saveUninitialized: true })
+// );
 
 server.use(passport.initialize());
-server.use(passport.session());
+// server.use(passport.session());
 
 server.use(express.urlencoded({ extended: false }));
 server.use(morgan('dev'));
