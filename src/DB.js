49d0a6c66335d412df1606c78313f0c3852b45e6
unknown <joakig6@gmail.com>
2023-10-10 23:30:56 -0300

    possible solution

--- src/DB.js
+++ src/DB.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST ,DB_NAME,POSTGRES_USER,POSTGRES_HOST,POSTGRES_PASSWORD,POSTGRES_DATABASE} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST ,DB_NAME} = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER??POSTGRES_USER}:${DB_PASSWORD??POSTGRES_PASSWORD}@${DB_HOST??POSTGRES_HOST}/${DB_NAME??POSTGRES_DATABASE}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectModule:require('pg')
   }
 );
 const basename = path.basename(__filename);
