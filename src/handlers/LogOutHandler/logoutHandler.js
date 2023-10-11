45b10437ad7030d568647c929c48412da490e591
unknown <joakig6@gmail.com>
2023-10-11 14:17:27 -0300

    probando cooookie

--- src/handlers/LogOutHandler/logoutHandler.js
+++ src/handlers/LogOutHandler/logoutHandler.js
@@ -2,17 +2,17 @@ const { response, ErrorResponse } = require('../../utils');
 
 module.exports = (req, res) => {
   try {
-    res.cookie('accesscookie', '', { expires: new Date(0) });
-    //  res.send({
-    //    error:false,
-    //    message:'Se ha deslogueado con exito'
-    //  })
+    res.cookie('accesscookie', '', {
+      expires:new Date(0)
+    });
+    console.log('me deslogueo de ',req.cookies.accesscookie);
     response(res, 200, { message: 'Se ha deslogueado con exito' });
   } catch (error) {
     //  res.status(401).send({
     //    error: true,
     //    message: error.message ?? 'server failed.',
     //  });
+    console.log(error);
     ErrorResponse(res, 401, error);
   }
 };
