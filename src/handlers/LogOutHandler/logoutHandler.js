6f6157644ae86d02fed06d592d0402de314fef2b
unknown <joakig6@gmail.com>
2023-10-15 21:35:11 -0300

    production test

--- src/handlers/LogOutHandler/logoutHandler.js
+++ src/handlers/LogOutHandler/logoutHandler.js
@@ -4,11 +4,24 @@ const { FRONT_DOMAIN } = process.env;
 module.exports = (req, res) => {
   try {
 
-    res.cookie('accesscookie', '', {
-      expires:new Date(0)
-    });
-    response(res, 200, { message: 'Se ha deslogueado con exito. '});
+    // res.cookie('accesscookie', '', {
+    //   expires:new Date(0)
+    // });
+
+    res.cookie('accesscookie','',{
+      expires:new Date(0),
+      httpOnly:true,
+      sameSite:'none',
+      secure:true,
+      domain:'joadev.com.ar',
+    })
+    response(res, 200, { message: 'Se ha deslogueado con exito en produccion 13.' });
   } catch (error) {
+    //  res.status(401).send({
+    //    error: true,
+    //    message: error.message ?? 'server failed.',
+    //  });
+    console.log(error);
     ErrorResponse(res, 401, error);
   }
 };
