e34f7d09687d2a2128ada0c6ffcbb8ed6c6a870e
unknown <joakig6@gmail.com>
2023-10-17 23:44:35 -0300

    Update error response in register handler,

--- src/handlers/UserHandlers/registerUserHandler.js
+++ src/handlers/UserHandlers/registerUserHandler.js
@@ -1,22 +1,10 @@
-const { createUser } = require('../../controllers/UserControllers');
-const { UniqueConstraintError } = require('sequelize');
-const { response, ErrorResponse } = require('../../utils');
+const { createUser } = require("../../controllers/UserControllers");
+const { response, ErrorResponse } = require("../../utils");
 module.exports = async (req, res) => {
   try {
     const info = await createUser(req.body);
-    // res.status(201).send(info);
-    response(res, 201, { data: info });
+    return response(res, 201, { data: info });
   } catch (error) {
-    if (error instanceof UniqueConstraintError) {
-      // res
-      //   .status(406)
-      //   .send('Este correo ya se encuentra en uso, por favor utilice otro.');
-      ErrorResponse(res, 406, {
-        message: 'Este correo ya se encuentra en uso, por favor utilice otro.',
-      });
-    } else {
-      // res.status(500).send('ERROR SERVER.');
-      ErrorResponse(res, 500, error);
-    }
+    return ErrorResponse(res, 401, error);
   }
 };
