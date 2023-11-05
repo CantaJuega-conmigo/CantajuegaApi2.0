2078ac69f50d54353e4966fb033b7f8c39e5e34e
unknown <joakig6@gmail.com>
2023-11-05 02:37:28 -0300

    some changes

--- src/controllers/MembershipContollers/getMembership.js
+++ src/controllers/MembershipContollers/getMembership.js
@@ -1,10 +1,10 @@
-const { Membership } = require("../../DB");
+const { Membership ,Stage} = require("../../DB");
 
 module.exports = async (id) => {
   try {
     const memberships = !id
-      ? await Membership.findAll({ include: ["Users"] })
-      : await Membership.findByPk(id, { include: ["Users"] });
+      ? await Membership.findAll({ include: ["Users",Stage] })
+      : await Membership.findByPk(id, { include: ["Users",Stage] });
     if (id && !memberships) throw new Error("Membresia no existe.");
     if (!id && !memberships.length)
       throw new Error("Membresias no encontradas.");
