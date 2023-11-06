ac48a564cb8a3c36240204f2b6274866d32b78dd
Wild-Design <brenneke_ruger@hotmail.com>
2023-11-06 20:38:26 -0300

    Add new Query

--- src/controllers/UserControllers/getUsersWithRelatedReports.js
+++ src/controllers/UserControllers/getUsersWithRelatedReports.js
@@ -1,12 +1,14 @@
 const { User, Report } = require('../../DB');
 
-module.exports = async () => {
+module.exports = async (id) => {
   try {
     const onlyUsersWithReports = await User.findAll({
       include: { model: Report, required: true },
     });
+
+    const getUser = await User.findByPk(id, { include: { model: Report } });
     //con required en true me aseguro de traer solo a los usuarios que tienen reportes
-    return onlyUsersWithReports;
+    return id ? getUser : onlyUsersWithReports;
   } catch (error) {
     throw new Error(
       `Error en el servidor 'getUsersWithRelatedReports': ${error.message}`
