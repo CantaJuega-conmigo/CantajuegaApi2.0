bb6370a9b9da7149754b22c4ff28b39d6b80c469
Wild-Design <brenneke_ruger@hotmail.com>
2023-11-23 17:44:01 -0300

    discoment validateauth and validateadmin

--- src/routes/Notifications.js
+++ src/routes/Notifications.js
@@ -10,11 +10,7 @@ const {
 
 const router = Router();
 
-router.get(
-  '/',
-  // , validateauth, validateAdmin
-  getNotificationsHandler
-);
+router.get('/', validateauth, validateAdmin, getNotificationsHandler);
 router.get('/:id', getNotificationsHandler);
 router.delete('/:id', deleteNotificationHandler);
 router.patch('/:id', updateNotificationHandler);
