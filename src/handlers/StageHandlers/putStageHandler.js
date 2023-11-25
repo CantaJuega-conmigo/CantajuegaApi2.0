ad43f4b92c50b6adc860db101759e08284a26fec
unknown <joakig6@gmail.com>
2023-11-25 20:21:24 -0300

    change to edit content stages

--- src/handlers/StageHandlers/putStageHandler.js
+++ src/handlers/StageHandlers/putStageHandler.js
@@ -15,13 +15,20 @@ module.exports = async (req, res) => {
     if (contentQuery && !orderQuery) {
       throw new Error("missing orderQuery");
     }
-    const { order, content, title } = req.body;
-    const newContent = { order, content, title };
-    const updated = !contentQuery
-      ? await putStage(stageId, newData)
-      : await editContentStage(stageId, contentQuery, newContent, orderQuery);
+    if (contentQuery && orderQuery) {
+      const { order, content, title } = req.body;
+      const newContent = { order, content, title };
+      const url = new URL(content);
+      const params = new URLSearchParams(url.search);
+      const vquery = params.get("v");
+      newContent.content = vquery;
+      const updated = await editContentStage(stageId, contentQuery, newContent, orderQuery);
+      return response(res, 200, { data: updated });
+    }
+    const updated = await putStage(stageId, newData);
     return response(res, 200, { data: updated });
   } catch (error) {
+    console.log(error);
     return ErrorResponse(res, 400, error);
   }
 };
