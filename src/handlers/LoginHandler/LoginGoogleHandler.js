4c1b459300682f9245b626bbbd5738166ff96431
unknown <joakig6@gmail.com>
2023-12-14 18:39:20 -0300

    google auth

--- src/handlers/LoginHandler/LoginGoogleHandler.js
+++ src/handlers/LoginHandler/LoginGoogleHandler.js
@@ -1,20 +1,30 @@
 const { createToken } = require("../../auth");
 const { createUser } = require("../../controllers/UserControllers");
 const { response, ErrorResponse } = require("../../utils");
-const { FRONT_DOMAIN ,FRONT_URL} = process.env;
+const { User } = require("../../DB");
+const { FRONT_DOMAIN, FRONT_URL } = process.env;
 module.exports = async (req, res) => {
   try {
-    const newUser = await createUser(req.user);
-    const token = createToken(newUser, "1d");
-    res.cookie("accesscookie",token, {
-      maxAge: 24 * 60 * 60 * 1000,
-      sameSite: "none",
-      secure: true,
-      httpOnly: true,
-      domain: FRONT_DOMAIN,
+    const userExist = await User.findOne({
+      where: { email: req.user.email },
+      include: ["Children", "Reports", "Membership"],
     });
-    res.redirect(FRONT_URL);// to do , redirect to register child page
+    const user = userExist ? userExist : await createUser(req.user);
+    const token = createToken(user, "1d");
+    if (!user?.Children?.length) {
+      return res.redirect(`${FRONT_URL}/complete-register`); //redirect if the user hasn't children
+    } else if (user?.Children?.length) {
+      //para cuando ya tiene hijos registrados
+      res.cookie("accesscookie", token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+        domain: FRONT_DOMAIN,
+      });
+      return res.redirect(FRONT_URL); //
+    }
   } catch (error) {
     ErrorResponse(res, 401, error);
   }
-}; 
+};
