3dc705114b4b8b92cfefc392243dbe568e05b332
unknown <joakig6@gmail.com>
2023-12-14 16:41:24 -0300

    google auth

--- src/routes/authgoogle.js
+++ src/routes/authgoogle.js
@@ -4,7 +4,7 @@ const {googleauth, googlecallback}=require('../middlewares');
 const { loginGoogleHandler } = require('../handlers/LoginHandler');
 router.get('/google',googleauth);///ruta donde se ejecutara el inico con google,
 router.get('/google/callback',googlecallback,loginGoogleHandler)// ruta donde se maneja la respuesta de la peticion, osea el inicio/registro de google
-router.get('/prueba',(req,res)=>{
-    res.send('hola')
+router.get('/googleredirect',(req,res)=>{
+    res.redirect('http://localhost:3001/api/auth/google')
 })
 module.exports=router
