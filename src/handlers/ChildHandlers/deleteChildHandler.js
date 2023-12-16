8489991f6f5d839d3732a621be9fa1857f3e34c4
unknown <joakig6@gmail.com>
2023-12-16 14:44:14 -0300

    reset deploy

--- src/handlers/ChildHandlers/deleteChildHandler.js
+++ src/handlers/ChildHandlers/deleteChildHandler.js
@@ -5,11 +5,9 @@ module.exports = async (req, res) => {
   const { id } = req.body;
   try {
     const deleted = await deleteChild(id);
-    // res.status(200).json({ message: "stage deleted", deleted });
     response(res, 200, { message: 'Stage deleted, deleted' });
   } catch (error) {
-    console.log(error);
-    // res.status(500).send('Error in the server');
+
     ErrorResponse(res, 500, error);
   }
 };
